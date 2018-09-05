ba7f892799f943bf2c397412b76bec72998d1386
Goran Jakovljevic <goranefbl@gmail.com>
2018-09-05 09:20:05 +0200

    Table Sort function.

--- src/components/Table/AM2Table.js
+++ src/components/Table/AM2Table.js
@@ -1,6 +1,17 @@
 import React, { Component } from 'react';
+import AM2TableSort from './AM2TableSort';
 
 class AM2Table extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            page: 1,
+            numPages: 1,
+            sortedBy: '',
+            sortMode: 'ASC'
+        };
+    }
+
     deep = (obj, path) => {
         try {
             return new Function('_', `return _.${path}`)(obj); // eslint-disable-line
@@ -9,21 +20,40 @@ class AM2Table extends Component {
         }
     };
 
+    sortColumn = index => {
+        this.setState(prevState => ({
+            sortedBy: index,
+            sortMode: prevState.sortMode === 'ASC' ? 'DESC' : 'ASC'
+        }));
+        console.log(this.state);
+    };
+
     render() {
-        const { data, columns } = this.props;
+        const { columns } = this.props;
+        let { rows } = this.props;
+        const { sortedBy, sortMode } = this.state;
+        // Sorting?
+        if (sortedBy) {
+            rows = AM2TableSort(rows, sortedBy, sortMode);
+        }
+
         return (
             <table className="table__table">
                 <thead className="table__header">
                     <tr className="table__row">
                         {columns.map(column => (
-                            <th className="table__heading text-left" key={column.key}>
+                            <th
+                                className="table__heading text-left"
+                                key={column.key}
+                                onClick={this.sortColumn.bind(this, column.key)}
+                            >
                                 {column.title}
                             </th>
                         ))}
                     </tr>
                 </thead>
                 <tbody className="table__body">
-                    {data.map(rowData => (
+                    {rows.map(rowData => (
                         <tr key={rowData.id} className="table__row">
                             {columns.map(element => (
                                 <td key={element.key} className="table__cell text-left">
@@ -39,3 +69,11 @@ class AM2Table extends Component {
 }
 
 export default AM2Table;
+
+AM2Table.defaultProps = {
+    rows: [],
+    columns: [],
+    pageSize: 10,
+    filter: [],
+    sort: []
+};
---
+++ src/components/Table/AM2TableSort.js
@@ -0,0 +1,72 @@
+/* eslint-disable */
+/* Need to disable due to naturalSort alghoritm */
+
+// http://www.overset.com/2008/09/01/javascript-natural-sort-algorithm/
+const naturalSort = (a, b) => {
+    var re = /(^-?[0-9]+(\.?[0-9]*)[df]?e?[0-9]?$|^0x[0-9a-f]+$|[0-9]+)/gi,
+        sre = /(^[ ]*|[ ]*$)/g,
+        dre = /(^([\w ]+,?[\w ]+)?[\w ]+,?[\w ]+\d+:\d+(:\d+)?[\w ]?|^\d{1,4}[\/\-]\d{1,4}[\/\-]\d{1,4}|^\w+, \w+ \d+, \d{4})/,
+        hre = /^0x[0-9a-f]+$/i,
+        ore = /^0/,
+        i = function(s) {
+            return (naturalSort.insensitive && ('' + s).toLowerCase()) || '' + s;
+        },
+        // convert all to strings strip whitespace
+        x = i(a).replace(sre, '') || '',
+        y = i(b).replace(sre, '') || '',
+        // chunk/tokenize
+        xN = x
+            .replace(re, '\0$1\0')
+            .replace(/\0$/, '')
+            .replace(/^\0/, '')
+            .split('\0'),
+        yN = y
+            .replace(re, '\0$1\0')
+            .replace(/\0$/, '')
+            .replace(/^\0/, '')
+            .split('\0'),
+        // numeric, hex or date detection
+        xD = parseInt(x.match(hre)) || (xN.length != 1 && x.match(dre) && Date.parse(x)),
+        yD = parseInt(y.match(hre)) || (xD && y.match(dre) && Date.parse(y)) || null,
+        oFxNcL,
+        oFyNcL;
+    // first try and sort Hex codes or Dates
+    if (yD)
+        if (xD < yD) return -1;
+        else if (xD > yD) return 1;
+    // natural sorting through split numeric strings and default strings
+    for (var cLoc = 0, numS = Math.max(xN.length, yN.length); cLoc < numS; cLoc++) {
+        // find floats not starting with '0', string or 0 if not defined (Clint Priest)
+        oFxNcL = (!(xN[cLoc] || '').match(ore) && parseFloat(xN[cLoc])) || xN[cLoc] || 0;
+        oFyNcL = (!(yN[cLoc] || '').match(ore) && parseFloat(yN[cLoc])) || yN[cLoc] || 0;
+        // handle numeric vs string comparison - number < string - (Kyle Adams)
+        if (isNaN(oFxNcL) !== isNaN(oFyNcL)) {
+            return isNaN(oFxNcL) ? 1 : -1;
+        }
+        // rely on string comparison if different types - i.e. '02' < 2 != '02' < '2'
+        else if (typeof oFxNcL !== typeof oFyNcL) {
+            oFxNcL += '';
+            oFyNcL += '';
+        }
+        if (oFxNcL < oFyNcL) return -1;
+        if (oFxNcL > oFyNcL) return 1;
+    }
+    return 0;
+};
+
+const AM2TableSort = (rows, sortedBy, sortMode) => {
+    const sorter = rows
+        .map((row, index) => ({
+            id: index,
+            val: row[sortedBy]
+        }))
+        .sort((a, b) => naturalSort(a.val, b.val))
+        .map(item => rows[item.id]);
+
+    if (sortMode === 'DESC') {
+        return sorter.reverse();
+    }
+    return sorter;
+};
+
+export default AM2TableSort;
