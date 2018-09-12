793a63d0ce11b05edeeeab7fabeff193fc0e4169
Goran Jakovljevic <goranefbl@gmail.com>
2018-09-12 19:39:01 +0200

    addNote Widget API

--- src/views/Dashboard/Widgets/AddNote.js
+++ src/views/Dashboard/Widgets/AddNote.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import Textarea from '../../../components/Form/Textarea';
 import Select from '../../../components/Form/Select';
+import WP_API from '../../../data/Api';
+import Notification from '../../../components/Form/Notification';
 
 import '../../../styles/custom.css';
 
@@ -9,8 +11,10 @@ class AddNote extends Component {
         super(props);
 
         this.state = {
-            user: '',
-            comment: ''
+            note_for: '',
+            note_type: '',
+            content: '',
+            status: false
         };
     }
 
@@ -19,31 +23,71 @@ class AddNote extends Component {
         this.setState({ [name]: value });
     };
 
-    render() {
-        const { companies } = this.props;
-        const { user, comment } = this.state;
+    closeNotification = () => {
+        this.setState(() => ({ status: false }));
+    };
 
+    addUserNote = () => {
+        const api = new WP_API();
+        api.setPost('user-note', '', this.state);
+        api.set().then(result => {
+            if (result.success === true) {
+                this.setState(() => ({ status: 'success' }));
+            } else {
+                this.setState(() => ({ status: 'error' }));
+                console.log('Something went wrong!');
+            }
+        });
+    };
+
+    render() {
+        const { users } = this.props;
+        const { note_for, content, note_type, status } = this.state; // eslint-disable-line  camelcase
+
+        const userList = users.map(user => ({
+            id: user.id,
+            title: `${user.first_name} ${user.last_name}`
+        }));
+
+        const noteType = [
+            { id: '0', title: 'Positive' },
+            { id: '1', title: 'Negative' },
+            { id: '2', title: 'Neutral' }
+        ];
         const inputs = [
             {
                 type: Select,
-                name: 'user',
+                name: 'note_for',
                 label: 'For user',
-                list: companies,
+                list: userList,
                 required: true,
-                value: user,
+                value: note_for,
+                parentClass: 'form__column col-1 form__row'
+            },
+            {
+                type: Select,
+                name: 'note_type',
+                label: 'Type',
+                list: noteType,
+                required: true,
+                value: note_type,
                 parentClass: 'form__column col-1 form__row'
             },
             {
                 type: Textarea,
-                name: 'comment',
+                name: 'content',
                 label: 'Comment',
                 rows: '4',
                 required: true,
-                value: comment,
+                value: content,
                 parentClass: 'form__column col-1 form__row'
             }
         ];
-
+        // Notification Text
+        let msgText = 'Thanks for the Note!';
+        if (status === 'error') {
+            msgText = 'Upss.. something went wrong! Check with Goran.';
+        }
         return (
             <div className="section col-14 widget widget--usernotes">
                 <header className="section__header">
@@ -52,6 +96,15 @@ class AddNote extends Component {
                 <div className="section__content">
                     <div className="widget">
                         <form className="form">
+                            {status ? (
+                                <Notification
+                                    text={msgText}
+                                    type={status}
+                                    close={this.closeNotification}
+                                />
+                            ) : (
+                                ''
+                            )}
                             <div className="form__row">
                                 {inputs.map(field => (
                                     <field.type
@@ -68,9 +121,15 @@ class AddNote extends Component {
                                     />
                                 ))}
                             </div>
-                            <button type="button" className="button button--primary button--custom">
-                                Submit
-                            </button>
+                            <div className="form__row">
+                                <button
+                                    type="button"
+                                    className="button button--primary button--custom"
+                                    onClick={this.addUserNote}
+                                >
+                                    Submit
+                                </button>
+                            </div>
                         </form>
                     </div>
                 </div>
