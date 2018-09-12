fcddce65994100a0883fc41e83b82a5f7b928b57
Goran Jakovljevic <goranefbl@gmail.com>
2018-09-12 20:06:27 +0200

    Vacation widget API

--- src/views/Dashboard/Widgets/RequestVacation.js
+++ src/views/Dashboard/Widgets/RequestVacation.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
+import WP_API from '../../../data/Api';
 import Text from '../../../components/Form/Text';
+import Textarea from '../../../components/Form/Textarea';
 import DatePicker from '../../../components/Form/DatePicker';
+import Notification from '../../../components/Form/Notification';
 
 import '../../../styles/custom.css';
 
@@ -9,9 +12,11 @@ class RequestVacation extends Component {
         super(props);
 
         this.state = {
-            startDate: '',
-            endDate: '',
-            days: ''
+            start_date: '',
+            end_date: '',
+            days: '',
+            note: '',
+            status: false
         };
     }
 
@@ -20,24 +25,41 @@ class RequestVacation extends Component {
         this.setState({ [name]: value });
     };
 
+    closeNotification = () => {
+        this.setState(() => ({ status: false }));
+    };
+
+    requestVacation = () => {
+        const api = new WP_API();
+        api.setPost('vacations', '', this.state);
+        api.set().then(result => {
+            if (result.success === true) {
+                this.setState(() => ({ status: 'success' }));
+            } else {
+                this.setState(() => ({ status: 'error' }));
+                console.log('Something went wrong!');
+            }
+        });
+    };
+
     render() {
-        const { startDate, endDate, days } = this.state;
+        const { start_date, end_date, days, note, status } = this.state; // eslint-disable-line camelcase
 
         const inputs = [
             {
                 type: DatePicker,
-                name: 'startDate',
+                name: 'start_date',
                 label: 'Start Date',
                 required: true,
-                value: startDate,
+                value: start_date,
                 parentClass: 'form__row'
             },
             {
                 type: DatePicker,
-                name: 'endDate',
+                name: 'end_date',
                 label: 'End Date',
                 required: true,
-                value: endDate,
+                value: end_date,
                 parentClass: 'form__row'
             },
             {
@@ -48,8 +70,22 @@ class RequestVacation extends Component {
                 required: true,
                 value: days,
                 parentClass: 'form__row'
+            },
+            {
+                type: Textarea,
+                name: 'note',
+                label: 'Comment',
+                rows: '4',
+                required: true,
+                value: note,
+                parentClass: 'form__column col-1 form__row'
             }
         ];
+        // Notification Text
+        let msgText = 'Request sent! We will get back to you soon.';
+        if (status === 'error') {
+            msgText = 'Upss.. something went wrong! Check with Goran.';
+        }
 
         return (
             <div className="section col-14 widget widget--vacation">
@@ -59,6 +95,15 @@ class RequestVacation extends Component {
                 <div className="section__content">
                     <div className="widget">
                         <form className="form">
+                            {status ? (
+                                <Notification
+                                    text={msgText}
+                                    type={status}
+                                    close={this.closeNotification}
+                                />
+                            ) : (
+                                ''
+                            )}
                             <div className="form__row">
                                 {inputs.map(field => (
                                     <field.type
@@ -76,7 +121,11 @@ class RequestVacation extends Component {
                                     />
                                 ))}
                             </div>
-                            <button type="button" className="button button--primary button--custom">
+                            <button
+                                type="button"
+                                className="button button--primary button--custom"
+                                onClick={this.requestVacation}
+                            >
                                 Submit
                             </button>
                         </form>
