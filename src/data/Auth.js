32d711e2c2275f4e27c861eab84efe8f42a63a72
Goran Jakovljevic <goranefbl@gmail.com>
2018-10-03 09:33:09 +0200

    Permissions check

--- src/data/Auth.js
+++ src/data/Auth.js
@@ -4,13 +4,21 @@ class WP_AUTH {
     constructor() {
         this.url = 'http://crm.am2studio.com/wp-json/jwt-auth/v1/token';
         this.validateUrl = 'http://crm.am2studio.com/wp-json/jwt-auth/v1/token/validate';
+        this.permissionsUrl = 'http://crm.am2studio.com/wp-json/crm/v2/permissions';
         this.tokenKey = 'crmTokenKey';
         this.userName = 'crmUserName';
+        this.permissions = 'permissions';
     }
 
     getSessionToken = () => sessionStorage.getItem(this.tokenKey);
 
-    removeSessionToken = () => sessionStorage.removeItem(this.tokenKey);
+    getPermissions = () =>
+        sessionStorage.getItem(this.permissions) ? sessionStorage.getItem(this.permissions) : '';
+
+    removeSessionToken = () => {
+        localStorage.clear();
+        sessionStorage.clear();
+    };
 
     isAuthenticated = () => {
         if (this.getSessionToken() === null) {
@@ -41,6 +49,19 @@ class WP_AUTH {
                 console.log(response);
                 sessionStorage.setItem(this.tokenKey, response.data.token);
                 sessionStorage.setItem(this.userName, response.data.user_display_name);
+                // Get role
+                return axios({
+                    method: 'post',
+                    url: this.permissionsUrl,
+                    headers: {
+                        'Content-Type': 'application/json',
+                        Authorization: `Bearer ${this.getSessionToken()}`
+                    }
+                });
+            })
+            .then(response => {
+                console.log(response);
+                sessionStorage.setItem(this.permissions, response.data);
                 return response.status;
             })
             .catch(error => {
