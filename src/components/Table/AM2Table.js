449e3cf01613aaac0364a356ee93497f01eb2702
Goran Jakovljevic <goranefbl@gmail.com>
2018-10-06 10:38:30 +0200

    Pagination - client side.

--- src/components/Table/AM2Table.js
+++ src/components/Table/AM2Table.js
@@ -1,18 +1,24 @@
 import React, { Component } from 'react';
 import AM2TableSort from './AM2TableSort';
 import AM2TableFilter from './AM2TableFilter';
+import AM2TablePagination from './AM2TablePagination';
 
 class AM2Table extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            page: 1, // eslint-disable-line
-            numPages: 1, // eslint-disable-line
+            currentPage: 1,
             sortedBy: '',
             sortMode: 'ASC',
             filter: undefined
         };
     }
+
+    onPageChanged = page => {
+        const { onPageChanged } = this.props;
+        this.setState(() => ({ currentPage: page }));
+        onPageChanged(page);
+    };
 
     deep = (obj, path) => {
         try {
@@ -48,9 +54,9 @@ class AM2Table extends Component {
     };
 
     render() {
-        const { columns } = this.props;
+        const { columns, itemsPerPage } = this.props;
         let { rows } = this.props;
-        const { sortedBy, sortMode, filter } = this.state;
+        const { sortedBy, sortMode, filter, currentPage } = this.state;
         // Sorting?
         if (sortedBy) {
             rows = AM2TableSort(rows, sortedBy, sortMode);
@@ -59,50 +65,59 @@ class AM2Table extends Component {
             rows = AM2TableFilter(rows, filter);
         }
 
+        rows = rows.slice(0, itemsPerPage);
+
         return (
-            <table className="table__table">
-                <thead className="table__header">
-                    <tr className="table__row">
-                        {columns &&
-                            columns.map(column => (
-                                <th
-                                    className="table__heading text-left"
-                                    key={column.key}
-                                    onClick={this.sortColumn.bind(this, column.key)}
-                                >
-                                    {column.title}
-                                </th>
+            <React.Fragment>
+                <table className="table__table">
+                    <thead className="table__header">
+                        <tr className="table__row">
+                            {columns &&
+                                columns.map(column => (
+                                    <th
+                                        className="table__heading text-left"
+                                        key={column.key}
+                                        onClick={this.sortColumn.bind(this, column.key)}
+                                    >
+                                        {column.title}
+                                    </th>
+                                ))}
+                        </tr>
+                        <tr className="table__row">
+                            {columns &&
+                                columns.map(column => (
+                                    <th key={column.key}>
+                                        <input
+                                            autoComplete="off"
+                                            type="text"
+                                            className="input__standard"
+                                            name={column.key}
+                                            onChange={e => this.filterColumn(e, column.key)}
+                                        />
+                                    </th>
+                                ))}
+                        </tr>
+                    </thead>
+                    <tbody className="table__body">
+                        {rows &&
+                            rows.map(rowData => (
+                                <tr key={rowData.id} className="table__row">
+                                    {columns &&
+                                        columns.map(element => (
+                                            <td key={element.key} className="table__cell text-left">
+                                                {this.deep(rowData, element.key)}
+                                            </td>
+                                        ))}
+                                </tr>
                             ))}
-                    </tr>
-                    <tr className="table__row">
-                        {columns &&
-                            columns.map(column => (
-                                <th key={column.key}>
-                                    <input
-                                        autoComplete="off"
-                                        type="text"
-                                        className="input__standard"
-                                        name={column.key}
-                                        onChange={e => this.filterColumn(e, column.key)}
-                                    />
-                                </th>
-                            ))}
-                    </tr>
-                </thead>
-                <tbody className="table__body">
-                    {rows &&
-                        rows.map(rowData => (
-                            <tr key={rowData.id} className="table__row">
-                                {columns &&
-                                    columns.map(element => (
-                                        <td key={element.key} className="table__cell text-left">
-                                            {this.deep(rowData, element.key)}
-                                        </td>
-                                    ))}
-                            </tr>
-                        ))}
-                </tbody>
-            </table>
+                    </tbody>
+                </table>
+                <AM2TablePagination
+                    numOfPages={Math.ceil(rows.length / itemsPerPage)}
+                    currentPage={currentPage}
+                    onPageChanged={this.onPageChanged}
+                />
+            </React.Fragment>
         );
     }
 }
---
+++ src/components/Table/AM2TablePagination.js
@@ -0,0 +1,132 @@
+import React, { Component } from 'react';
+
+class AM2TablePagination extends Component {
+    constructor(props) {
+        super(props);
+        const { numOfPages } = props;
+        this.state = { currentPage: 1 };
+
+        this.numOfPages = numOfPages;
+        this.pageNeighbours = 1;
+    }
+
+    range = (from, to, step = 1) => {
+        let i = from;
+        const range = [];
+
+        while (i <= to) {
+            range.push(i);
+            i += step;
+        }
+
+        return range;
+    };
+
+    gotoPage = page => {
+        const { onPageChanged = f => f } = this.props;
+        this.setState({ currentPage: page });
+        onPageChanged(page);
+    };
+
+    fetchPageNumbers = () => {
+        const { numOfPages, pageNeighbours } = this;
+        const { currentPage } = this.state;
+
+        const totalNumbers = this.pageNeighbours * 2 + 3;
+        const totalBlocks = totalNumbers + 2;
+
+        if (numOfPages > totalBlocks) {
+            let pages = [];
+
+            const leftBound = currentPage - pageNeighbours;
+            const rightBound = currentPage + pageNeighbours;
+            const beforeLastPage = numOfPages - 1;
+
+            const startPage = leftBound > 2 ? leftBound : 2;
+            const endPage = rightBound < beforeLastPage ? rightBound : beforeLastPage;
+
+            pages = this.range(startPage, endPage);
+
+            const pagesCount = pages.length;
+            const singleSpillOffset = totalNumbers - pagesCount - 1;
+
+            const leftSpill = startPage > 2;
+            const rightSpill = endPage < beforeLastPage;
+
+            const leftSpillPage = 'LEFT';
+            const rightSpillPage = 'RIGHT';
+
+            if (leftSpill && !rightSpill) {
+                const extraPages = this.range(startPage - singleSpillOffset, startPage - 1);
+                pages = [leftSpillPage, ...extraPages, ...pages];
+            } else if (!leftSpill && rightSpill) {
+                const extraPages = this.range(endPage + 1, endPage + singleSpillOffset);
+                pages = [...pages, ...extraPages, rightSpillPage];
+            } else if (leftSpill && rightSpill) {
+                pages = [leftSpillPage, ...pages, rightSpillPage];
+            }
+
+            return [1, ...pages, numOfPages];
+        }
+
+        return this.range(1, numOfPages);
+    };
+
+    render() {
+        const { currentPage } = this.state;
+        const pages = this.fetchPageNumbers();
+        return (
+            <div className="dataTables_paginate paging_simple_numbers">
+                {pages.map(page => {
+                    if (page === 'LEFT')
+                        return (
+                            <button
+                                key={page}
+                                type="button"
+                                className={
+                                    currentPage === page
+                                        ? 'paginate_button active'
+                                        : 'paginate_button'
+                                }
+                                tabIndex={page}
+                                onClick={() => this.gotoPage(page)}
+                            >
+                                <span aria-hidden="true">&laquo;</span>
+                            </button>
+                        );
+
+                    if (page === 'RIGHT')
+                        return (
+                            <button
+                                key={page}
+                                type="button"
+                                className={
+                                    currentPage === page
+                                        ? 'paginate_button active'
+                                        : 'paginate_button'
+                                }
+                                tabIndex={page}
+                                onClick={() => this.gotoPage(page)}
+                            >
+                                <span aria-hidden="true">&raquo;</span>
+                            </button>
+                        );
+                    return (
+                        <button
+                            type="button"
+                            className={`page-paginate_button${
+                                currentPage === page ? ' active' : ''
+                            }`}
+                            tabIndex={page}
+                            onClick={() => this.gotoPage(page)}
+                        >
+                            {page}
+                        </button>
+                    );
+                })}
+            </div>
+        );
+    }
+}
+
+export default AM2TablePagination;
