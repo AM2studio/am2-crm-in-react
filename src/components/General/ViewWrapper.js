e575664528ad61dc54b5a8b5e1f971ad7108c56b
Goran Jakovljevic <goranefbl@gmail.com>
2018-10-07 23:48:12 +0200

    remove header animation

--- src/components/General/ViewWrapper.js
+++ src/components/General/ViewWrapper.js
@@ -1,20 +1,14 @@
 import React from 'react';
-import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 
 const ComponentHeader = props => {
     const { title, children } = props;
     return (
-        <ReactCSSTransitionGroup
-            transitionAppear
-            transitionName="loadComponent"
-            transitionEnterTimeout={500}
-            transitionLeaveTimeout={300}
-        >
+        <React.Fragment>
             <header className="section__header">
                 <h2 className="section__title">{title}</h2>
             </header>
             <div className="section__content">{children}</div>
-        </ReactCSSTransitionGroup>
+        </React.Fragment>
     );
 };
 
