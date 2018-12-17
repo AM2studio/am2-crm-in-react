c0b47c8ec074d4c50fb843e478637891f0f32f8b
Goran Jakovljevic <goranefbl@gmail.com>
2018-12-17 01:32:44 +0100

    prettify

--- src/views/ProjectReports/ProjectReportsContainer.js
+++ src/views/ProjectReports/ProjectReportsContainer.js
@@ -62,97 +62,87 @@ class ProjectReportsContainer extends Component {
         const byPassCache = true;
         const byPassCacheSave = true;
         const api = new WP_API();
-        const projectReports = api
-            .getPosts(
-                'project-reports',
-                {
-                    itemsPerPage: 20000,
-                    filterProject,
-                    filterDepartment,
-                    filterJobType,
-                    filterDate
-                },
-                byPassCache,
-                byPassCacheSave
-            )
-            .then(result => {
-                const posts = result.data
-                    ? result.data.map(post => ({
-                          id: post.id,
-                          billable_hours: post.billable_hours,
-                          month: post.month,
-                          user: post.user,
-                          project: post.project,
-                          milestones: post.milestones,
-                          milestone: post.milestone,
-                          project_feature: post.project_feature,
-                          date: post.date,
-                          // hours: post.hours,
-                          job_type: post.job_type,
-                          comment: post.comment,
-                          asana_url: post.asana_url
-                      }))
-                    : false;
-                return posts
-                    ? {
-                          posts,
-                          totalRecords: result.data ? result.data.count : 0,
-                          totalHours: result.report.data.totals.hours,
-                          userData: {
-                              labels: result.report.data.totals.users.labels,
-                              datasets: [
-                                  {
-                                      data: result.report.data.totals.users.hours,
-                                      backgroundColor: this.generateRandomColors(
-                                          result.report.data.totals.users.hours.length
-                                      )
-                                  }
-                              ]
-                          },
-                          jobTypeData: {
-                              labels: result.report.data.totals.job_type.labels,
-                              datasets: [
-                                  {
-                                      data: result.report.data.totals.job_type.hours,
-                                      backgroundColor: this.generateRandomColors(
-                                          result.report.data.totals.job_type.hours.length
-                                      )
-                                  }
-                              ]
-                          },
-                          milestoneData: {
-                              labels: result.report.data.totals.milestones.labels,
-                              datasets: [
-                                  {
-                                      data: result.report.data.totals.milestones.hours,
-                                      backgroundColor: this.generateRandomColors(
-                                          result.report.data.totals.milestones.hours.length
-                                      )
-                                  }
-                              ]
-                          },
-                          barChartData: Object.values(result.report.data.totals.users.dates)
-                      }
-                    : false;
-            });
-
-        Promise.all([projectReports]).then(values => {
-            this.setState({
-                projectReports: values[0].posts ? values[0].posts : [],
-                totalRecords: values[0].totalRecords,
-                totalHours: values[0].totalHours,
-                userData: values[0].userData,
-                jobTypeData: values[0].jobTypeData,
-                milestoneData: values[0].milestoneData,
-                loading: false,
-                barChartData: values[0].barChartData
-                    ? values[0].barChartData.map(bar => ({
-                          ...bar,
-                          backgroundColor: randomColor({ luminosity: 'bright' })
-                      }))
-                    : [],
-                empty: values[0].totalRecords === 0
-            });
+        api.getPosts(
+            'project-reports',
+            {
+                itemsPerPage: 20000,
+                filterProject,
+                filterDepartment,
+                filterJobType,
+                filterDate
+            },
+            byPassCache,
+            byPassCacheSave
+        ).then(result => {
+            const posts =
+                result.data &&
+                result.data.map(post => ({
+                    id: post.id,
+                    billable_hours: post.billable_hours,
+                    month: post.month,
+                    user: post.user,
+                    project: post.project,
+                    milestones: post.milestones,
+                    milestone: post.milestone,
+                    project_feature: post.project_feature,
+                    date: post.date,
+                    // hours: post.hours,
+                    job_type: post.job_type,
+                    comment: post.comment,
+                    asana_url: post.asana_url
+                }));
+            if (!posts) {
+                this.setState({
+                    loading: false
+                });
+            } else {
+                this.setState({
+                    projectReports: posts,
+                    totalRecords: result.data ? result.data.count : 0,
+                    totalHours: result.report.data.totals.hours,
+                    userData: {
+                        labels: result.report.data.totals.users.labels,
+                        datasets: [
+                            {
+                                data: result.report.data.totals.users.hours,
+                                backgroundColor: this.generateRandomColors(
+                                    result.report.data.totals.users.hours.length
+                                )
+                            }
+                        ]
+                    },
+                    jobTypeData: {
+                        labels: result.report.data.totals.job_type.labels,
+                        datasets: [
+                            {
+                                data: result.report.data.totals.job_type.hours,
+                                backgroundColor: this.generateRandomColors(
+                                    result.report.data.totals.job_type.hours.length
+                                )
+                            }
+                        ]
+                    },
+                    milestoneData: {
+                        labels: result.report.data.totals.milestones.labels,
+                        datasets: [
+                            {
+                                data: result.report.data.totals.milestones.hours,
+                                backgroundColor: this.generateRandomColors(
+                                    result.report.data.totals.milestones.hours.length
+                                )
+                            }
+                        ]
+                    },
+                    barChartData: Object.values(result.report.data.totals.users.dates)
+                        ? Object.values(result.report.data.totals.users.dates).map(bar => ({
+                              ...bar,
+                              backgroundColor: randomColor({ luminosity: 'bright' })
+                          }))
+                        : [],
+                    empty: result.totalRecords === 0,
+                    loading: false
+                });
+            }
         });
     };
 
@@ -162,6 +152,21 @@ class ProjectReportsContainer extends Component {
             this.getData();
         });
     };
+
+    hours = (hour, billable_hours) => <p data-tip={`billable: ${billable_hours}`}>{hour}</p>; // eslint-disable-line camelcase
+
+    date = (date, month) => <p data-tip={month}>{date}</p>;
+
+    filterJobType = jobType => {
+        const jobTypeClass = jobType.replace(/[^a-zA-Z]+/g, '');
+        return <span className={`jobtype ${jobTypeClass}`}>{jobType}</span>;
+    };
+
+    asana = asana => (
+        <a href={asana} rel="noopener noreferrer" target="_blank" data-tip={asana}>
+            {asana}
+        </a>
+    );
 
     render() {
         const {
@@ -180,8 +185,17 @@ class ProjectReportsContainer extends Component {
             milestoneData,
             barChartData
         } = this.state;
+
+        const filteredData = projectReports.map(entry => ({
+            ...entry,
+            job_type: this.filterJobType(entry.job_type),
+            hours: this.hours(entry.hours, entry.billable_hours),
+            date: this.date(entry.date, entry.month),
+            asana_url: entry.asana_url && this.asana(entry.asana_url)
+            //    buttons: this.actionBtns(entry.id)
+        }));
+
         const columns = [
-            { key: 'month', title: 'Month' },
             { key: 'user', title: 'User' },
             { key: 'billable_hours', title: 'Billable Hours' },
             { key: 'date', title: 'Date' },
@@ -246,7 +260,7 @@ class ProjectReportsContainer extends Component {
                 {barChartData.length ? <UserPerDateChart data={barChartData} /> : ''}
                 <ProjectReports
                     columns={columns}
-                    data={projectReports}
+                    data={filteredData}
                     totalRecords={totalRecords}
                     empty={empty}
                 />
@@ -256,18 +270,3 @@ class ProjectReportsContainer extends Component {
 }
 
 export default ProjectReportsContainer;
-
-ProjectReportsContainer.defaultProps = {
-    itemsPerPage: 20
-};
-
-// Userchart example
-// usersChart: {
-//     labels: ['Red', 'Green', 'Yellow'],
-//     datasets: [
-//         {
-//             data: [300, 50, 100],
-//             backgroundColor: ['#FF6384', '#36A2EB', '#FFCE56']
-//         }
-//     ]
-// },
