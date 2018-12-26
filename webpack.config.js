0190766212f0c7c1d7b8e0680d7bcb21be23ff1f
Goran Jakovljevic <goranefbl@gmail.com>
2018-12-26 15:46:32 +0100

    webpack config in a single file.
    No need for separate files when its small configuration. Use of env variable to load modules per different env.

--- webpack.config.js
+++ webpack.config.js
@@ -11,54 +11,66 @@ const htmlPlugin = new HTMLWebPackPlugin({
     filename: './index.html'
 });
 
-module.exports = {
-    entry: { main: './src/index.js' },
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: '[name].[hash].js'
-    },
-    // optimization: {
-    //     runtimeChunk: 'single',
-    //     splitChunks: {
-    //         chunks: 'all',
-    //         maxInitialRequests: Infinity,
-    //         minSize: 0
-    //     }
-    // },
-    module: {
-        rules: [
-            {
-                test: /.jsx?$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader'
+module.exports = (env, options) => {
+    console.log(options);
+    return {
+        entry: { main: './src/index.js' },
+        output: {
+            path: path.resolve(__dirname, 'dist'),
+            filename: options.mode === 'production' ? '[name].[hash].js' : '[name].js'
+        },
+        // optimization: {
+        //     runtimeChunk: 'single',
+        //     splitChunks: {
+        //         chunks: 'all',
+        //         maxInitialRequests: Infinity,
+        //         minSize: 0
+        //     }
+        // },
+        module: {
+            rules: [
+                {
+                    test: /.jsx?$/,
+                    exclude: /node_modules/,
+                    use: {
+                        loader: 'babel-loader'
+                    }
+                },
+                {
+                    test: /\.js$/,
+                    exclude: /node_modules/,
+                    use: ['babel-loader', 'eslint-loader']
+                },
+                {
+                    test: /\.css$/,
+                    use: [
+                        options.mode === 'production'
+                            ? MiniCssExtractPlugin.loader
+                            : 'style-loader',
+                        'css-loader'
+                    ]
+                },
+                {
+                    test: /\.(jpg|png|svg|gif)$/,
+                    loader: 'file-loader',
+                    options: {
+                        name: '[path][name].[hash].[ext]'
+                    }
                 }
-            },
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: ['babel-loader', 'eslint-loader']
-            },
-            {
-                test: /\.css$/,
-                use: [MiniCssExtractPlugin.loader, 'css-loader']
-            },
-            {
-                test: /\.(jpg|png|svg|gif)$/,
-                loader: 'file-loader',
-                options: {
-                    name: '[path][name].[hash].[ext]'
-                }
-            }
-        ]
-    },
-    plugins: [
-        new CleanWebpackPlugin('dist', {}),
-        new MiniCssExtractPlugin({
-            filename: 'style.[hash].css'
-        }),
-        new AsyncChunkNames(),
-        htmlPlugin,
-        new WebpackMd5Hash()
-    ]
+            ]
+        },
+        plugins: [
+            new CleanWebpackPlugin('dist', {}),
+            new MiniCssExtractPlugin({
+                filename: 'style.[hash].css'
+            }),
+            new AsyncChunkNames(),
+            htmlPlugin,
+            new WebpackMd5Hash()
+        ],
+        devtool: 'cheap-module-eval-source-map',
+        devServer: {
+            historyApiFallback: true
+        }
+    };
 };
