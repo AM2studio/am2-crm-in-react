bd4ce560572ac137e34d1d08b0e1a6a358e240fb
Goran Jakovljevic <goranefbl@gmail.com>
2018-12-31 08:42:41 +0100

    webpack config to single file.

--- webpack.config.js
+++ webpack.config.js
@@ -11,66 +11,77 @@ const htmlPlugin = new HTMLWebPackPlugin({
     filename: './index.html'
 });
 
-module.exports = (env, options) => {
-    console.log(options);
-    return {
-        entry: { main: './src/index.js' },
-        output: {
-            path: path.resolve(__dirname, 'dist'),
-            filename: options.mode === 'production' ? '[name].[hash].js' : '[name].js'
-        },
-        // optimization: {
-        //     runtimeChunk: 'single',
-        //     splitChunks: {
-        //         chunks: 'all',
-        //         maxInitialRequests: Infinity,
-        //         minSize: 0
-        //     }
-        // },
-        module: {
-            rules: [
-                {
-                    test: /.jsx?$/,
-                    exclude: /node_modules/,
-                    use: {
-                        loader: 'babel-loader'
-                    }
-                },
-                {
-                    test: /\.js$/,
-                    exclude: /node_modules/,
-                    use: ['babel-loader', 'eslint-loader']
-                },
-                {
-                    test: /\.css$/,
-                    use: [
-                        options.mode === 'production'
-                            ? MiniCssExtractPlugin.loader
-                            : 'style-loader',
-                        'css-loader'
-                    ]
-                },
-                {
-                    test: /\.(jpg|png|svg|gif)$/,
-                    loader: 'file-loader',
-                    options: {
-                        name: '[path][name].[hash].[ext]'
-                    }
+module.exports = (env, options) => ({
+    entry: { main: './src/index.js' },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: options.mode === 'production' ? '[name].[hash].js' : '[name].js'
+    },
+    // optimization: {
+    //     runtimeChunk: 'single',
+    //     splitChunks: {
+    //         cacheGroups: {
+    //             default: false,
+    //             vendors: false,
+    //             // vendor chunk
+    //             vendor: {
+    //                 // sync + async chunks
+    //                 chunks: 'all',
+    //                 // import file path containing node_modules
+    //                 test: /node_modules/
+    //             },
+    //             common: {
+    //                 name: 'common',
+    //                 minChunks: 2,
+    //                 chunks: 'async',
+    //                 priority: 10,
+    //                 reuseExistingChunk: true,
+    //                 enforce: true
+    //             }
+    //         }
+    //     }
+    // },
+    module: {
+        rules: [
+            {
+                test: /.jsx?$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
                 }
-            ]
-        },
-        plugins: [
-            new CleanWebpackPlugin('dist', {}),
-            new MiniCssExtractPlugin({
-                filename: 'style.[hash].css'
-            }),
-            new AsyncChunkNames(),
-            htmlPlugin,
-            new WebpackMd5Hash()
-        ],
-        devtool: 'cheap-module-eval-source-map',
-        devServer: {
-            historyApiFallback: true
-        }
-    };
-};
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: ['babel-loader', 'eslint-loader']
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    options.mode === 'production' ? MiniCssExtractPlugin.loader : 'style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.(jpg|png|svg|gif)$/,
+                loader: 'file-loader',
+                options: {
+                    name: '[path][name].[hash].[ext]'
+                }
+            }
+        ]
+    },
+    plugins: [
+        new CleanWebpackPlugin('dist', {}),
+        new MiniCssExtractPlugin({
+            filename: 'style.[hash].css'
+        }),
+        new AsyncChunkNames(),
+        htmlPlugin,
+        new WebpackMd5Hash()
+    ],
+    devtool: 'cheap-module-eval-source-map',
+    devServer: {
+        historyApiFallback: true
+    }
+});
