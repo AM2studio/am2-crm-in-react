4228d002d729771a24d49f8740707f3e16599ab3
Goran Jakovljevic <goranefbl@gmail.com>
2019-01-01 23:32:31 +0100

    rewriting dashboard with new context API.

--- src/views/Dashboard/Dashboard.js
+++ src/views/Dashboard/Dashboard.js
@@ -1,60 +1,19 @@
-import React, { Component } from 'react';
+import React from 'react';
 import AddTimeEntry from './Widgets/AddTimeEntry';
 import AddNote from './Widgets/AddNote';
 import AddHighFive from './Widgets/AddHighFive';
 import RequestVacation from './Widgets/RequestVacation';
-import WP_API from '../../data/Api';
+import { SharedDataConsumer } from '../../data/SharedDataContext';
 
-class Dashboard extends Component {
-    constructor() {
-        super();
-        this.state = {
-            projects: [],
-            users: []
-        };
-    }
-
-    componentDidMount() {
-        // Get Companies
-        const api = new WP_API();
-        // Get Users
-        api.getPosts('users').then(result => {
-            const posts = result.data.map(post => ({
-                id: post.id,
-                first_name: post.first_name,
-                last_name: post.last_name,
-                title: `${post.first_name} ${post.last_name}`,
-                company_role: post.company_role,
-                department: post.department,
-                email: post.email,
-                role: post.role
-            }));
-            this.setState({ users: posts });
-        });
-
-        // Get Projects
-        api.getPosts('projects').then(result => {
-            const posts = result.data.map(post => ({
-                id: post.id,
-                title: post.title,
-                company: post.company_name
-            }));
-            this.setState({ projects: posts });
-        });
-
-        // Goran: Prebaciti u jedan call sa promise.all
-    }
-
-    render() {
-        const { users, projects } = this.state;
-        return (
+export default () => (
+    <SharedDataConsumer>
+        {({ users, projects }) => (
             <React.Fragment>
                 <AddTimeEntry projects={projects} />
                 <RequestVacation />
                 <AddNote users={users} />
                 <AddHighFive users={users} />
             </React.Fragment>
-        );
-    }
-}
-export default Dashboard;
+        )}
+    </SharedDataConsumer>
+);
